364a17b05f18f09a4f7f3e4c748ce96f2c82c30d
Miritigacheri <138796435+Miritigacheri@users.noreply.github.com>
2024-04-06 19:52:18 +0300

    Edits to signup form

--- src/pages/signup/signUpForm.jsx
+++ src/pages/signup/signUpForm.jsx
@@ -7,16 +7,21 @@ import google from "../../assets/Google.png";
 import { Link } from "react-router-dom";
 
 const SignUpForm = () => {
-  const { register, handleSubmit, errors } = useForm();
-  const { userInfo, setUserInfo } = useState();
-  const onSubmit = (data) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [userInfo, setUserInfo] = useState();
+  const onSubmit = (data, e) => {
     setUserInfo(data);
+    e.target.reset();
+    alert("Account created successfully, proceed to Login");
     console.log(data);
   };
 
   return (
     <div className="main">
-      <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
       <div className="leftSide">
         <div className="nav">
           <Link to="/">Home</Link>
@@ -42,11 +47,13 @@ const SignUpForm = () => {
             <input
               type="text"
               name="username"
-              id="Email"
+              id="username"
               placeholder="username"
-              ref={register({ required: "username is required" })}
+              {...register("username", {
+                required: "Username is a required field",
+              })}
             />
-            <br />
+            <p id="error">{errors.username?.message}</p>
             <br />
             <label htmlFor="Email">Email Address</label>
             <br />
@@ -55,9 +62,12 @@ const SignUpForm = () => {
               name="Email"
               id="Email"
               placeholder="Johndoe@gmail.com"
-              ref={register({ required: "email is required" })}
+              {...register("email", {
+                required: "Email address is a required field",
+              })}
             />
-            <br />
+            <p id="error">{errors.email?.message}</p>
+
             <br />
             <label htmlFor="Password">Password</label>
             <br />
@@ -66,8 +76,11 @@ const SignUpForm = () => {
               name="Password"
               id="Password"
               placeholder="password"
-              ref={...register(""{ required: true })}
+              {...register("password", {
+                required: "Password is a required field",
+              })}
             />
+            <p id="error">{errors.password?.message}</p>
             <br />
             <br />
             <button type="submit">Sign Up</button>
