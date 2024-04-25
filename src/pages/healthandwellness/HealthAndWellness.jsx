6700d65ddec11b5b43017d01463672eafaa7c873
Miritigacheri <138796435+Miritigacheri@users.noreply.github.com>
2024-04-25 06:22:49 +0300

    changes to health and wellness UI

--- src/pages/healthandwellness/HealthAndWellness.jsx
+++ src/pages/healthandwellness/HealthAndWellness.jsx
@@ -1,11 +1,197 @@
 import React from "react";
 import Topbar from "../../components/navbar/Topbar";
+import style from "../../pages/healthandwellness/healthandwellness.module.css";
+import baby from "../../assets/baby.jpg";
 
 const HealthAndWellness = () => {
   return (
     <>
       <Topbar />
-      <div className="px-[10px] py-[10px] lg:px-[80px] ">Health And Wellness Page</div>
+      <div className={style.banner}>
+        <h1 className={style.title}>Health And Wellness </h1>
+      </div>
+      <div className={style.main}>
+        <div className={style.articlebody}>
+          <div className={style.articleimg}>
+            <img src={baby} alt="" />
+          </div>
+          <div className={style.articlecontent}>
+            <h4>How to care for your baby</h4>
+            <p>
+              {" "}
+              Caring for babies is a rewarding and delicate responsibility...{" "}
+              <a>Read more</a>
+            </p>
+
+            <span>Contributed by Dr Raheed Paul</span>
+          </div>
+        </div>
+        <div className={style.articlebody}>
+          <div className={style.articleimg}>
+            <img src={baby} alt="" />
+          </div>
+          <div className={style.articlecontent}>
+            <h4>How to care for your baby</h4>
+            <p>
+              {" "}
+              Caring for babies is a rewarding and delicate responsibility...{" "}
+              <a>Read more</a>
+            </p>
+
+            <span>Contributed by Dr Raheed Paul</span>
+          </div>
+        </div>
+        <div className={style.articlebody}>
+          <div className={style.articleimg}>
+            <img src={baby} alt="" />
+          </div>
+          <div className={style.articlecontent}>
+            <h4>How to care for your baby</h4>
+            <p>
+              {" "}
+              Caring for babies is a rewarding and delicate responsibility...{" "}
+              <a>Read more</a>
+            </p>
+
+            <span>Contributed by Dr Raheed Paul</span>
+          </div>
+        </div>
+        <div className={style.articlebody}>
+          <div className={style.articleimg}>
+            <img src={baby} alt="" />
+          </div>
+          <div className={style.articlecontent}>
+            <h4>How to care for your baby</h4>
+            <p>
+              {" "}
+              Caring for babies is a rewarding and delicate responsibility...{" "}
+              <a>Read more</a>
+            </p>
+
+            <span>Contributed by Dr Raheed Paul</span>
+          </div>
+        </div>
+        <div className={style.articlebody}>
+          <div className={style.articleimg}>
+            <img src={baby} alt="" />
+          </div>
+          <div className={style.articlecontent}>
+            <h4>How to care for your baby</h4>
+            <p>
+              {" "}
+              Caring for babies is a rewarding and delicate responsibility...{" "}
+              <a>Read more</a>
+            </p>
+
+            <span>Contributed by Dr Raheed Paul</span>
+          </div>
+        </div>
+        <div className={style.articlebody}>
+          <div className={style.articleimg}>
+            <img src={baby} alt="" />
+          </div>
+          <div className={style.articlecontent}>
+            <h4>How to care for your baby</h4>
+            <p>
+              {" "}
+              Caring for babies is a rewarding and delicate responsibility...{" "}
+              <a>Read more</a>
+            </p>
+
+            <span>Contributed by Dr Raheed Paul</span>
+          </div>
+        </div>
+        <div className={style.articlebody}>
+          <div className={style.articleimg}>
+            <img src={baby} alt="" />
+          </div>
+          <div className={style.articlecontent}>
+            <h4>How to care for your baby</h4>
+            <p>
+              {" "}
+              Caring for babies is a rewarding and delicate responsibility...{" "}
+              <a>Read more</a>
+            </p>
+
+            <span>Contributed by Dr Raheed Paul</span>
+          </div>
+        </div>
+        <div className={style.articlebody}>
+          <div className={style.articleimg}>
+            <img src={baby} alt="" />
+          </div>
+          <div className={style.articlecontent}>
+            <h4>How to care for your baby</h4>
+            <p>
+              {" "}
+              Caring for babies is a rewarding and delicate responsibility...{" "}
+              <a>Read more</a>
+            </p>
+
+            <span>Contributed by Dr Raheed Paul</span>
+          </div>
+        </div>
+        <div className={style.articlebody}>
+          <div className={style.articleimg}>
+            <img src={baby} alt="" />
+          </div>
+          <div className={style.articlecontent}>
+            <h4>How to care for your baby</h4>
+            <p>
+              {" "}
+              Caring for babies is a rewarding and delicate responsibility...{" "}
+              <a>Read more</a>
+            </p>
+
+            <span>Contributed by Dr Raheed Paul</span>
+          </div>
+        </div>
+        <div className={style.articlebody}>
+          <div className={style.articleimg}>
+            <img src={baby} alt="" />
+          </div>
+          <div className={style.articlecontent}>
+            <h4>How to care for your baby</h4>
+            <p>
+              {" "}
+              Caring for babies is a rewarding and delicate responsibility...{" "}
+              <a>Read more</a>
+            </p>
+
+            <span>Contributed by Dr Raheed Paul</span>
+          </div>
+        </div>
+        <div className={style.articlebody}>
+          <div className={style.articleimg}>
+            <img src={baby} alt="" />
+          </div>
+          <div className={style.articlecontent}>
+            <h4>How to care for your baby</h4>
+            <p>
+              {" "}
+              Caring for babies is a rewarding and delicate responsibility...{" "}
+              <a>Read more</a>
+            </p>
+
+            <span>Contributed by Dr Raheed Paul</span>
+          </div>
+        </div>
+        <div className={style.articlebody}>
+          <div className={style.articleimg}>
+            <img src={baby} alt="" />
+          </div>
+          <div className={style.articlecontent}>
+            <h4>How to care for your baby</h4>
+            <p>
+              {" "}
+              Caring for babies is a rewarding and delicate responsibility...{" "}
+              <a>Read more</a>
+            </p>
+
+            <span>Contributed by Dr Raheed Paul</span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
